543accce58a448bf94f16615070c74cb0540d1ac
Nnamdi Michael Okpala <okpalan@protonmail.com>
2024-09-24 12:49:23 +0100

    saved-it-for-later-with-config

--- ParaCreator.Tests.ps1
+++ ParaCreator.Tests.ps1
@@ -1,22 +1,6 @@
+
 # Import the module to test
-function Import-CorrectModule {
-    param (
-        [Parameter(Mandatory = $true)]
-        [string]$ModulePath
-    )
-
-    if (-not (Test-Path -Path $ModulePath)) {
-        $ModulePath = Join-Path -Path (Get-Location) -ChildPath $ModulePath
-    }
-
-    if (Test-Path -Path $ModulePath) {
-        Import-Module -Name $ModulePath -Force
-        Write-Host "Successfully imported module: $ModulePath"
-    } else {
-        Write-Host "Module not found: $ModulePath" -ForegroundColor Red
-    }
-}
-
+Import-Module -Name (Join-Path -Path $PSScriptRoot -ChildPath "./Scripts/ImportCorrectModule.psm1")
 Import-CorrectModule -ModulePath (Join-Path -Path $PSScriptRoot -ChildPath "ParaCreator.psd1")
 
 Describe "New-ParaStructure" {
